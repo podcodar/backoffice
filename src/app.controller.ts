7dce93a0805fcfaf8e3d48fdd88df67eb0c92a1e
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-30 01:36:34 -0300

    ref: logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Inject, injector, Query } from 'danet/mod.ts';
 import { Render } from 'danet/src/renderer/decorator.ts';
 import { CounterService } from './services/counter/counter.service.ts';
-import { DB_CONNECTION } from '~/services/database/db.shared.ts';
 
 @Controller('')
 export class AppController {
@@ -27,11 +26,10 @@ export class AppController {
   @Render('visits')
   @Get('/visits')
   async visits(@Query('name') name = 'visitant') {
-    console.log(injector.get(DB_CONNECTION));
-    console.log();
     const counterService = injector.get(CounterService);
-    console.log(injector.getAll());
-    console.log(injector.getAll().get(CounterService));
+    const map = injector.getAll();
+    console.log(map);
+    console.log(map?.get(CounterService));
     await counterService.addVisit();
     const visits = await counterService.getVisits();
     return { visits, name };
