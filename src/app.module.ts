6b34ecfc55b0b44a0ead487c93acc4fa645fbb32
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-30 01:21:56 -0300

    fix: add injectable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from 'danet/mod.ts';
 import { AppController } from './app.controller.ts';
 import { CounterModule } from './services/counter/counter.module.ts';
 import { AuthModule } from './services/auth/auth.module.ts';
+import { CounterService } from '~/services/counter/counter.service.ts';
 
 @Module({
   controllers: [AppController],
   imports: [CounterModule, AuthModule],
+  injectables: [CounterService],
 })
 export class AppModule {}
