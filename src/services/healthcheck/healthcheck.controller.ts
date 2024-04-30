02739e0918c90e0ed2671f22bf8d3939bcc94cd3
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-30 09:48:08 -0300

    fix: health check name

--- src/services/healthcheck/healthcheck.controller.ts
+++ src/services/healthcheck/healthcheck.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from 'danet/mod.ts';
 @Controller('/healthcheck')
 export class HealthCheckController {
   @Get('/')
-  async helloWorld() {
+  async healthcheck() {
     return 'OK';
   }
 }
