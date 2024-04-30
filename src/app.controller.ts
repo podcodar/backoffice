bcb9315616d4eb6f94064fe0d7bb555b85b53150
Marco Antônio <ma.souza.junior@gmail.com>
2024-04-29 21:59:05 -0300

    enh: add logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Query } from 'danet/mod.ts';
+import { Controller, Get, Inject, Query } from 'danet/mod.ts';
 import { Render } from 'danet/src/renderer/decorator.ts';
 import { CounterService } from '~/services/counter/counter.service.ts';
 
 @Controller('')
 export class AppController {
   constructor(
-    private readonly counterService: CounterService,
+    @Inject() private readonly counterService: CounterService,
   ) {}
 
   @Render('hello')
@@ -23,6 +23,7 @@ export class AppController {
   @Render('visits')
   @Get('/visits')
   async visits(@Query('name') name = 'visitant') {
+    console.log({ name, a: this });
     await this.counterService.addVisit();
     const visits = await this.counterService.getVisits();
     return { visits, name };
